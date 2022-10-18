713942e50caf9eab0bde517c2eb5ecff5dfd06f9
Alex <al.korotkov@intranet-sintek.net>
2022-10-18 16:33:35 +0300

    edit header button style

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,18 +13,18 @@ function Header(props) {
   switch (location.pathname) {
     case "/sign-in":
       block = (
-        <Link to="/sign-up" className="header__button">Регистрация</Link>
+        <Link to="/sign-up" className="header__button header__button_type_register">Регистрация</Link>
       )
       break;
     case "/sign-up":
 
       block = (
-        <Link to="/sign-in" className="header__button">Войти</Link>
+        <Link to="/sign-in" className="header__button header__button_type_login">Войти</Link>
       )
       break;
     case "/":
       block = (
-        <button className="header__button" onClick={handleClick}>Выйти</button>
+        <button className="header__button header__button_type_exit" onClick={handleClick}>Выйти</button>
       )
       break;
   }
