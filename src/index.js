885c1244ccf800b59e1302d910af5395ccc48b8b
Alex <al.korotkov@intranet-sintek.net>
2022-10-21 08:53:08 +0300

    edit styles with scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/styles.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
