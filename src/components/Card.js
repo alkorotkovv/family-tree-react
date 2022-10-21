2a9f0dc0cbbae5988e0fe9a45f15ec0999931ac0
Alex <al.korotkov@intranet-sintek.net>
2022-10-21 13:15:41 +0300

    refactoring styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -44,7 +44,7 @@ function Card(props) {
           <img className="card__image" src={props.card.image.imageUrl} alt="фотография" />
           <h2 className={cardTitleClassName}>{props.card.name}</h2>        
       </div>
-      <button className="card__delete card__delete_visible" type="button" aria-label="Delete" onClick={handleCardDeleteClick} title="Удалить" />
+      <button className="card__delete" type="button" aria-label="Delete" onClick={handleCardDeleteClick} title="Удалить" />
     </li>
   )
 }
